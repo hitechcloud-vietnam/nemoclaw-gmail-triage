ba4111e670ff85bbdfc882bd10d4a388545b610a
Nguyen Thanh An <73547738+thanhan92-f1@users.noreply.github.com>
2026-07-22 00:44:09 +0700

    Create index.ts

---
+++ src/gmail/index.ts
@@ -0,0 +1,3 @@
+export { getAuthClient, runOAuthFlow, isAuthenticated, clearCachedClient } from "./auth.js";
+export * from "./client.js";
+export { getScopes, SCOPES_BASE, SCOPE_SEND } from "./scopes.js";
